185ea64c8a007dcade15d412cf669574774fbde6
Carlos Hernández <CarlosHdz_97@outlook.com>
2019-06-02 10:51:40 -0500

    Ultimos cambios

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   publicPath: '/Desired_Schedule/',
   outputDir: 'docs',
-  assetsDir: undefined,
-  runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
-  css: undefined
 }
