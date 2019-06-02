f31f5435939945c6d7e21de268e4f5146061f133
Carlos Hernández <CarlosHdz_97@outlook.com>
2019-06-02 12:42:19 -0500

    Disponibilidad

--- src/components/DisponibilidadForm.vue
+++ src/components/DisponibilidadForm.vue
@@ -2,14 +2,38 @@
     <div>
         <b-form class="p-4 m-2">
             <b-form-group label="Turno: ">
-                <b-form-select v-model="form.turno" :options="options_turno"></b-form-select>
+                <b-form-select v-model="form.turno" :options="options_turno" @change="generateHorario"></b-form-select>
             </b-form-group>
         </b-form>
         <b-row>
-            <b-col cols="12" md="8">
-                <b-tabs content-class="mt-3" fill>
-                    <b-tab v-for="dia in options_days" :key="dia.value" :title="dia.text"><p>{{dia.text}}</p></b-tab>
-                </b-tabs>
+            <b-col cols="4" md="2" class="mt-5">
+                <b-list-group class="text-center">
+                    <b-list-group-item v-for="horas in horario" :key="horas.value">{{horas.value + ':00 Hrs'}}</b-list-group-item>
+                </b-list-group>
+            </b-col>
+            <b-col cols="8" md="10">
+                 <b-table striped hover :items="dias"></b-table>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="12">
+                <p class="h3 text-dark">Lunes</p>
+                <hr>
+                <div class="table-responsive">
+                    <b-card-group deck>
+                        <b-card bg-variant="primary" text-variant="white" header="Primary" class="text-center">
+                            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+                        </b-card>
+
+                        <b-card bg-variant="secondary" text-variant="white" header="Secondary" class="text-center">
+                            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+                        </b-card>
+
+                        <b-card bg-variant="success" text-variant="white" header="Success" class="text-center">
+                            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+                        </b-card>
+                    </b-card-group>
+                </div>
             </b-col>
         </b-row>
     </div>
@@ -18,11 +42,9 @@
 export default {
     data(){
         return{
+            horario:[],
             form:{
-                dia : '',
-                inicio: '07:00',
-                fin : '14:00',
-                turno : ''
+                turno:'Matutino'
             },
             options_turno:[
                 {value: "Matutino", text: "Matutino"},
@@ -35,7 +57,35 @@ export default {
                 {value: "Miercoles", text: "Miercoles"},
                 {value: "Jueves", text: "Jueves"},
                 {value: "Viernes", text: "Viernes"},
+            ],
+            dias:[
+                {'Día': "Lunes", horario:[]},
+                {'Día': "Martes", horario:[]},
+                {'Día': "Miercoles", horario:[]},
+                {'Día': "Jueves", horario:[]},
+                {'Día': "Viernes", horario:[]},
             ]
+        }
+    },
+    created(){
+        this.generateHorario()
+    },
+    methods:{
+        generateHorario(){
+            this.horario= [];
+            if(this.form.turno=="Matutino"){
+                for(let i=7; i<15; i++){
+                    this.horario.push({value:i});
+                }
+            }else if(this.form.turno=="Vespertino"){
+                for(let i=15; i<22; i++){
+                    this.horario.push({value:i});
+                }
+            }else{
+                for(let i=7; i<22; i++){
+                    this.horario.push({value:i});
+                }
+            }
         }
     }
 }
