59c6c5cdc0e13a24f217bcaab89786c74ff487c5
CarlosHz97 <CarlosHdz_97@outlook.com>
2019-06-03 19:12:11 -0500

    Mejoras en la vista

--- src/components/LogInForm.vue
+++ src/components/LogInForm.vue
@@ -1,21 +1,21 @@
 <template>
     <div>
-        <form class="p-3">
-            <div class="form-group">
-                <label>Correo electrónico <strong class="text-info">(*)</strong>:</label>
+        <form class="p-3"  @submit="logIn">
+            <b-form-group>
+                <label>{{txt_input_email}} <strong class="text-info">(*)</strong>:</label>
                 <input type="email" class="form-control" placeholder="Ingrese su correo electrónico" required>
-            </div>
-            <div class="form-group">
+            </b-form-group>
+            <div class="form-group" v-show="!recoveryPassword">
                 <label>Contraseña <strong class="text-info">(*)</strong>:</label>
                 <div class="input-group">
-                    <input :type="type_password" v-model="password" class="form-control" placeholder="Ingrese su cpntraseña" required>
+                    <input :type="type_password" v-model="password" class="form-control" placeholder="Ingrese su contraseña" required>
                     <span class="input-group-text" @click="revealPassword"><font-awesome-icon :icon="ic_password"/></span>
                 </div>
-                <router-link to="/reset-password" class="nav-link text-right">No recuerdo mi contraseña</router-link>
+                <p class="text-right text-primary mt-1"><u style="cursor:pointer;" @click="showRecoveryForm">No recuerdo mi contraseña</u></p>
             </div>
             <div class="text-right">
-                <button type="button" class="btn btn btn-primary" @click="logIn">Aceptar</button>
-                <router-link to="/" class="btn btn btn-danger ml-3">Cancelar</router-link>
+                <button type="submit" class="btn btn btn-primary">Aceptar</button>
+                <b-button class="btn btn btn-danger ml-3" @click="cancelForm">Cancelar</b-button>
             </div>
         </form>
     </div>
@@ -25,9 +25,11 @@ export default {
     name: 'LogInForm',
     data(){
         return{
-            ic_password:'eye-slash',
-            type_password:'password',
-            password:''
+            txt_input_email: 'Correo electrónico',
+            recoveryPassword: false,
+            ic_password: 'eye-slash',
+            type_password: 'password',
+            password: ''
         }
     },
     methods:{
@@ -41,6 +43,18 @@ export default {
             }else{
                 this.ic_password = 'eye';
                 this.type_password = 'text';
+            }
+        },
+        showRecoveryForm(){
+            this.txt_input_email = 'Correo electrónico de la cuenta que desea recuperar';
+            this.recoveryPassword =  true;
+        },
+        cancelForm(){
+            if(this.recoveryPassword == true){
+                this.txt_input_email = 'Correo electrónico';
+                this.recoveryPassword =  false;
+            }else{
+                this.$router.push({path:'/'});
             }
         }
     }
---
+++ src/components/PreferenceUserForm0.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="row">
+    <div class="col-12">
+      <h3>Transition</h3>
+      <draggable
+        class="list-group"
+        tag="ul"
+        v-model="list"
+        v-bind="dragOptions"
+        @start="isDragging = true"
+        @end="isDragging = false"
+      >
+        <transition-group type="transition" name="flip-list">
+          <li
+            class="list-group-item"
+            v-for="element in list"
+            :key="element.order"
+          >
+            <i
+              :class="
+                element.fixed ? 'fa fa-anchor' : 'glyphicon glyphicon-pushpin'
+              "
+              @click="element.fixed = !element.fixed"
+              aria-hidden="true"
+            ></i>
+            {{ element.name }}
+          </li>
+        </transition-group>
+      </draggable>
+    </div>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ list }}</pre>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import draggable from "vuedraggable";
+const message = [
+  "vue.draggable",
+  "draggable",
+  "component",
+  "for",
+  "vue.js 2.0",
+  "based",
+  "on",
+  "Sortablejs"
+];
+export default {
+  name: "transition-example",
+  display: "Transition",
+  order: 6,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      list: message.map((name, index) => {
+        return { name, order: index + 1 };
+      })
+    };
+  },
+  methods: {
+    sort() {
+      this.list = this.list.sort((a, b) => a.order - b.order);
+    }
+  },
+  computed: {
+    dragOptions() {
+      return {
+        animation: 0,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost"
+      };
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  margin-top: 35px;
+}
+.flip-list-move {
+  transition: transform 0.5s;
+}
+.no-move {
+  transition: transform 0s;
+}
+.ghost {
+  opacity: 0.5;
+  background: #c8ebfb;
+}
+.list-group {
+  min-height: 20px;
+}
+.list-group-item {
+  cursor: move;
+}
+.list-group-item i {
+  cursor: pointer;
+}
+</style>
